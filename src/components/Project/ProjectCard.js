f7d43635eae3e3b2bc07834fe99c69b937a472f0
Joeun Park <park49j@mtholyoke.edu>
2023-03-07 15:32:55 -0500

    Update invalid url

--- src/components/Project/ProjectCard.js
+++ src/components/Project/ProjectCard.js
@@ -8,7 +8,9 @@ import "./ProjectCard.css"
 
 function ProjectCard({thumbnail, title, type, desc, url}) {
     return(
-        <a className='projectcard-container padding-bottom-48' href={url} target='_blank' rel="noreferrer" /*onClick={() => window.location.href=url}*/>
+        // if url is empty string, then don't make it clickable
+        url === ''
+        ? (<div className='projectcard-container padding-bottom-48'>
             <div className='flex justify-center thumbnail-container'>
                 <img src={thumbnail} alt='project-thumbnail' width="100%" />
             </div>
@@ -17,7 +19,17 @@ function ProjectCard({thumbnail, title, type, desc, url}) {
                 <p className='padding-top-8 italic'>{type}</p>
                 <p className='padding-top-4 font-light'>{desc}</p>
             </div>
-        </a>
+        </div>):
+        (<a className='projectcard-container padding-bottom-48' href={url} target='_blank' rel="noreferrer" /*onClick={() => window.location.href=url}*/>
+            <div className='flex justify-center thumbnail-container'>
+                <img src={thumbnail} alt='project-thumbnail' width="100%" />
+            </div>
+            <div>
+                <h3 className='padding-top-8'>{title}</h3>
+                <p className='padding-top-8 italic'>{type}</p>
+                <p className='padding-top-4 font-light'>{desc}</p>
+            </div>
+        </a>)
     )
 }
 
--- src/components/Project/ProjectItems.js
+++ src/components/Project/ProjectItems.js
@@ -1,6 +1,108 @@
 export const ProjectItems = [
     // Thumbnail dimesion: 640 × 480
     {
+      thumbnail: 'https://i.im.ge/2023/02/09/aN4rAq.Seed-io.png',
+      title: 'Seed.io',
+      type: 'Kotlin, Firebase',
+      desc: 'An android app that empowers startup builders to post their projects and receive feedback',
+      url: 'https://github.com/inception-tree/seed.io',
+      category: 'mobile-android',
+    },
+    {
+      thumbnail: 'https://i.im.ge/2023/02/09/a6lZEr.Twitter-Clone.png',
+      title: 'Jnitter',
+      type: 'Swift, Twitter API',
+      desc: 'Twitter clone app to view, compose, favorite, and retweet tweets',
+      url: 'https://github.com/jonipark/Jnitter',
+      category: 'mobile-ios',
+    },
+    {
+      thumbnail: 'https://i.im.ge/2023/02/09/aN4N76.Minesweeper.png',
+      title: 'Minesweeper',
+      type: 'Kotlin',
+      desc: 'An android app for minesweeper game',
+      //TODO: update github link
+      url: '',
+      category: 'mobile-android',
+    },
+    {
+      thumbnail: 'https://i.im.ge/2023/02/09/a6OHqJ.Instagram-Clone.png',
+      title: 'Jonistagram',
+      type: 'Swift, Parse',
+      desc: 'Instagram clone with a custom Parse backend that allows a user to post photos, view feed, and add comments',
+      url: 'https://github.com/jonipark/Jonistagram',
+      category: 'mobile-ios',
+    },
+    {
+      thumbnail: 'https://i.im.ge/2023/02/09/a6F3V6.IMDB-Clone.png',
+      title: 'Flix',
+      type: 'Swift, The Movie Database API',
+      desc: 'Flix is an app that allows users to browse movies from the The Movie Database API',
+      url: 'https://github.com/jonipark/Flix',
+      category: 'mobile-ios',
+    },
+    {
+      //TODO: higher resolution thumbnail / update to Medium blog url
+      thumbnail: 'https://i.im.ge/2022/08/16/OO97Op.IMG-0658.jpg',
+      title: 'Marron Mirror',
+      type: 'Raspberry Pi, JavaScript',
+      desc: 'Smart mirror that displays curated user information',
+      url: 'https://github.com/marron-sojo/MMM-ASL',
+      category: 'others',
+    },
+    {
+      thumbnail: 'https://i.im.ge/2023/02/09/aPUVBy.ShoppingList.png',
+      title: 'MyShoppingList',
+      type: 'Kotlin',
+      desc: 'An android app for shopping checklist',
+      //TODO: update github link
+      url: '',
+      category: 'mobile-android',
+    },
+    {
+      thumbnail: 'https://i.im.ge/2023/02/09/aNBwu1.Wallet.png',
+      title: 'MyWallet',
+      type: 'Kotlin',
+      desc: 'An android app for ledger management',
+      //TODO: update github link
+      url: '',
+      category: 'mobile-android',
+    },
+    {
+      thumbnail: 'https://i.im.ge/2022/08/16/OO0cDz.template.png',
+      title: 'SingSangSong',
+      type: 'Swift, Realm',
+      desc: 'Memo app for my go-to karaoke songs',
+      url: 'https://github.com/jonipark/SingSangSong',
+      category: 'mobile-ios',
+    },
+    {
+      thumbnail: 'https://i.im.ge/2023/02/10/aPCQzG.Stopwatch.png',
+      title: 'Stopwatch',
+      type: 'Kotlin, Thread',
+      desc: 'An android app for stopwatch',
+      //TODO: update github link
+      url: '',
+      category: 'mobile-android',
+    },
+    {
+      thumbnail: 'https://i.im.ge/2023/02/10/aPEwE6.Calculator.png',
+      title: 'Calulator',
+      type: 'Kotlin',
+      desc: 'An android app for calulator',
+      //TODO: update github link
+      url: '',
+      category: 'mobile-android',
+    },
+    {
+      thumbnail: 'https://i.im.ge/2022/08/16/OO55k9.must-seum.png',
+      title: 'Must-seum',
+      type: 'React JS, OpenStreetMap',
+      desc: 'Hackathon Project: museum must-see artworks recommendation',
+      url: 'https://must-seum.web.app/',
+      category: 'web',
+    },
+    {
         thumbnail: 'https://i.im.ge/2022/08/16/OO5iqF.mohoho.png',
         title: 'Mohoho',
         type: 'Html, CSS',
@@ -16,47 +118,6 @@ export const ProjectItems = [
       desc: 'Informative website for school CS organization',
       url: 'https://cssociety.web.app/',
       category: 'web',
-    },
-    {
-      thumbnail: 'https://i.im.ge/2022/08/16/OO55k9.must-seum.png',
-      title: 'Must-seum',
-      type: 'React JS, OpenStreetMap',
-      desc: 'Hackathon Project: museum must-see artworks recommendation',
-      url: 'https://must-seum.web.app/',
-      category: 'web',
-    },
-    {
-      thumbnail: 'https://i.im.ge/2022/08/16/OOjefK.twitter.png',
-      title: 'Twitter Clone',
-      type: 'iOS(Swift), Twitter API',
-      desc: 'Basic twitter clone app to view, compose, favorite, and retweet tweets',
-      url: 'https://github.com/jonipark/twitter_ios_starter_',
-      category: 'mobile',
-    },
-    {
-    thumbnail: 'https://i.im.ge/2022/08/16/OOjBd6.flix.png',
-    title: 'Flix',
-    type: 'iOS(Swift), The Movie Database API',
-    desc: 'Flix is an app that allows users to browse movies from the The Movie Database API',
-    url: 'https://github.com/jonipark/flix',
-    category: 'mobile',
-    },
-    {
-      thumbnail: 'https://i.im.ge/2022/08/16/OO0cDz.template.png',
-      title: 'SingSangSong',
-      type: 'iOS(Swift), Realm',
-      desc: 'Memo app for my go-to karaoke songs',
-      url: 'https://apps.apple.com/us/app/singsangsong-%EC%94%BD%EC%8C%A9%EC%8F%AD/id1485081089?l=us',
-      category: 'mobile',
-    },
-    {
-      //TODO: higher resolution thumbnail / update to Medium blog url
-      thumbnail: 'https://i.im.ge/2022/08/16/OO97Op.IMG-0658.jpg',
-      title: 'Marron Mirror',
-      type: 'Raspberry Pi, JavaScript',
-      desc: 'Smart mirror that displays curated user information',
-      url: 'https://github.com/marron-sojo/MMM-ASL',
-      category: 'others',
     },
     {
       thumbnail: 'https://i.im.ge/2022/08/16/OOmt3y.AngryFlappyBird.png',
@@ -75,12 +136,16 @@ export const buttons = [
       value: "all"
     },
     {
-      name: "Web",
-      value: "web"
+      name: "Android",
+      value: "mobile-android"
     },
     {
-      name: "Mobile",
-      value: "mobile"
+      name: "iOS",
+      value: "mobile-ios"
+    },
+    {
+      name: "Web",
+      value: "web"
     },
     {
       name: "Others",
